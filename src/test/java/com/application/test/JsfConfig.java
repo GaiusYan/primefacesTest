a3ef03306310df685d970304bfcceaba52b9e11e
gaiusYan <gaiusyanbena11@gmail.com>
2024-11-22 14:53:41 +0100

    configuration pas encore bonne

--- src/test/java/com/application/test/JsfConfig.java
+++ src/test/java/com/application/test/JsfConfig.java
@@ -1,4 +1,4 @@
-/* import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,24 +10,23 @@ public class JsfConfig {
     /**
      * Enregistre la servlet JSF pour traiter les requêtes `.xhtml`.
      */
-/*     @Bean
+    @Bean
     public ServletRegistrationBean<FacesServlet> facesServletRegistration() {
         ServletRegistrationBean<FacesServlet> registration = 
                 new ServletRegistrationBean<>(new FacesServlet(), "*.xhtml");
         registration.setLoadOnStartup(1);
         return registration;
-    } */
+    }
 
     /**
      * Configure des paramètres spécifiques pour JSF, si nécessaire.
      * Cela peut inclure des paramètres pour Mojarra, PrimeFaces, etc.
      */
-  /*   @Bean
+    @Bean
     public javax.servlet.FilterRegistrationBean<?> configureJsfFilter() {
         javax.servlet.FilterRegistrationBean<?> filterRegistrationBean = 
                 new javax.servlet.FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new com.sun.faces.config.ConfigureListener());
         return filterRegistrationBean;
     }
-} */
- 
+}
